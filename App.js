a50e7d9d4026ae75a7f469817f5f971a9aea47d3
sunil19042004 <108710362+sunil19042004@users.noreply.github.com>
2023-04-05 15:29:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Loginpage from './components/loginpage';
+
+function App() {
+  return (
+    <div>
+   <Loginpage/>
+    </div>
+  );
+}
+
+export default App;
